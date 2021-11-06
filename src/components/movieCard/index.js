f58ba3aa9c89fd16c2cff490eca30701ee939696
Jacq0 <59923922+Jacq0@users.noreply.github.com>
2021-11-06 17:48:30 +0000

    Refactor: Favourite movie list now stored in a context

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext  } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
@@ -15,6 +15,7 @@ import Grid from "@material-ui/core/Grid";
 import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
+import { MoviesContext } from "../../contexts/moviesContext";
 
 const useStyles = makeStyles({
   card: { maxWidth: 345 },
--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import SiteHeader from './components/siteHeader';
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools'
+import MoviesContextProvider from "./contexts/moviesContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -21,22 +22,26 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
-    return (
-      <QueryClientProvider client={queryClient}>
+  return (
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <SiteHeader />      {/* New Header  */}
-        <Switch>
-        <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
+        <SiteHeader />
+        <MoviesContextProvider>
+            {" "}
+            <Switch>
+            <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
           <Route path="/reviews/:id" component={MovieReviewPage} />
           <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
           <Route path="/movies/:id" component={MoviePage} />
           <Route exact path="/" component={HomePage} />
           <Redirect from="*" to="/" />
-        </Switch>
+            </Switch>
+        </MoviesContextProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-    );
-  };
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
